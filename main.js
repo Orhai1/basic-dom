e875309f5511a5d65d04fa8529c1be026809ed27
Or Haibi <orhaibi1@gmail.com>
2025-07-25 15:32:20 +0300

    exe1

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-// console.log(document)
+const ball = document.getElementById("ball")
+
+const moveRight = function () {
+  const left = parseInt(ball.style.left) || 0
+  ball.style.left = (left + 15) + "px"
+}
+
+const moveLeft = function () {
+  const left = parseInt(ball.style.left) || 0
+  ball.style.left = (left - 15) + "px"
+}
+
+const moveUp = function () {
+  const top = parseInt(ball.style.top) || 0
+  ball.style.top = (top - 15) + "px"
+}
+
+const moveDown = function () {
+  const top = parseInt(ball.style.top) || 0
+  ball.style.top = (top + 15) + "px"
+}
+
+
+document.getElementById("right").onclick = moveRight
+document.getElementById("left").onclick = moveLeft
+document.getElementById("up").onclick = moveUp
+document.getElementById("down").onclick = moveDown
